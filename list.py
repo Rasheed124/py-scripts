bf5e023e0904ae245748cffc788a6e71978cc091
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-09-08 17:18:30 +0100

    working on built in data structures in python + array

---
+++ list.py
@@ -0,0 +1,4 @@
+us_presidents_list = ['Joe Biden', 'Donald Trump', 'Barack Obama', 'Barack Obama', 'George W. Bush', 'George W. Bush']
+
+for president in us_presidents_list:
+    print(president)
--- set.py
+++ set.py
@@ -1,3 +1,22 @@
+
+
+# ========================================== Set Difference ======================================== #
+# ========================================== Set Symmetric Difference ======================================== #
+
+# The set symmetric difference operator returns the elements that are unique to either A or B, ignoring the elements that are common to both sets.
+
+A = {1, 2, 4, 6, 8}
+B = {1, 2, 3, 4, 5}
+ 
+print(A.symmetric_difference(B))
+ 
+# Output:
+# {3, 5, 6, 8}
+
+
+# ========================================== Supersets, Subsets, and Membership Using Python Sets ======================================== #
+
+
 # A = {1, 2, 3, 4, 5}
 # B = {1, 2, 4}
  
@@ -8,10 +27,17 @@
 # # Output: True
 
 
+# ========================================== Creating and Manipulating Sets in Python ======================================== #
 
-# Creating and Manipulating Sets in Python
-
-
+S = set()
+ 
+S.add(2)
+print(S)
+# Output: {2}
+ 
+S.remove(2)
+print(S)
+# Output: set()
 
 S1 = {1, 2, 3}
 S2 = S1.copy()  # independent copy of S1
@@ -24,7 +50,7 @@ print(S2)
 # Output: {1, 2, 3}
 
 
-# Where Can You Use Python Sets?
+# ========================================== Where Can You Use Python Sets? ======================================== #
 
 
 # A common example is to remove duplicates from a list by transforming it into a set and back into a list:
@@ -36,3 +62,34 @@ print(unique_values)
 # Output: [1, 2, 3, 4]
 
 # Another common application for sets is filtering data. Let’s say that you have three lists of IDs: one of all of your clients, and two others of clients who bought product X or Y:
+
+
+all_clients = {104, 203, 255, 289, 448}
+clients_bought_X = {104, 448}
+clients_bought_Y = {104, 255, 289}
+
+
+
+# Let’s try answering the following questions:
+
+# Which clients bought product X and did not buy product Y?
+# Which clients bought both products?
+# Which clients bought nothing?
+
+
+all_clients = {104, 203, 255, 289, 448}
+clients_bought_X = {104, 448}
+clients_bought_Y = {104, 255, 289}
+ 
+# Question 1
+print(clients_bought_X.difference(clients_bought_Y))
+# Output: {448}
+ 
+# Question 2
+print(clients_bought_X.intersection(clients_bought_Y))
+# Output: {104}
+ 
+# Question 3
+purchasers = clients_bought_X.union(clients_bought_Y)
+print(all_clients.difference(purchasers))
+# Output: {203}

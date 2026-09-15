3c2b35b1fa91e9f3942469855254c34db289068f
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-09-15 09:24:50 +0100

    working on advanced functions

--- function.py
+++ function.py
@@ -7,8 +7,8 @@
 # function()
 
 
-def calculate_cost(item="bananas", quantity=6, price=0.74):
-    print(f"{quantity} {item} cost ${quantity * price:.2f}")
+# def calculate_cost(item="bananas", quantity=6, price=0.74):
+#     print(f"{quantity} {item} cost ${quantity * price:.2f}")
 
 # calculate_cost("bananas", 0.74, 6)
 # calculate_cost(price=0.74, quantity=6, item="bananas")
@@ -21,10 +21,10 @@ def calculate_cost(item="bananas", quantity=6, price=0.74):
 # Returning Values
 
 
-def as_dict():
-     return dict(one=1, two=2, three=3)
+# def as_dict():
+#      return dict(one=1, two=2, three=3)
 
-print(as_dict()["two"])
+# print(as_dict()["two"])
 # {'one': 1, 'two': 2, 'three': 3}
 
 # >>> as_dict()["two"]
@@ -83,20 +83,45 @@ print(as_dict()["two"])
 # Returning Generator Iterators
 
 
-def cumulative_average(numbers):
-     total = 0
-     for items, number in enumerate(numbers, 1):
-         total += number
-         yield total / items
+# def cumulative_average(numbers):
+#      total = 0
+#      for items, number in enumerate(numbers, 1):
+#          total += number
+#          yield total / items
 
 
 values = [5, 3, 8, 2, 5]  # Simulates a large data set
 
-for cum_average in cumulative_average(values):
-     print(f"Cumulative average: {cum_average:.2f}")
+# for cum_average in cumulative_average(values):
+#      print(f"Cumulative average: {cum_average:.2f}")
 
 # Cumulative average: 5.00
 # Cumulative average: 4.00
 # Cumulative average: 5.33
 # Cumulative average: 4.50
 # Cumulative average: 4.60
+
+
+# def append_to(item, target=[]):
+#   target.append(item)
+#   return target
+
+
+
+# def append_to(item, target=[]):
+#     target.append(item)
+#     return target
+
+# print(append_to(2))
+# print(append_to(3))
+# print(append_to(4))
+
+
+
+def average(*args):
+    return sum(args) 
+
+
+args = average(1, 2, 3)
+
+print(args)

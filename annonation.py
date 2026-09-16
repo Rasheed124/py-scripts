265e3b54b92f4af0595f61986634908bf62468f2
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-09-16 18:30:11 +0100

    working on annonation

--- annonation.py
+++ annonation.py
@@ -44,8 +44,107 @@ john = {
 
 result = get_full_name(john)
 
-print(result)
+# print(result)
 
 
 
 # MutableMapping
+
+
+
+from collections.abc import MutableMapping
+
+def update_first_name(student: MutableMapping[str, str], first_name: str) -> None:
+    student["first_name"] = first_name
+
+john = {
+    "first_name": "John",
+    "last_name": "Doe",
+}
+
+update_first_name(john, "james")
+
+# print(john)
+
+
+
+# Using the TypedDict class as a type hint
+
+
+from typing import TypedDict
+
+class StudentDict(TypedDict):
+    first_name: str
+    last_name: str
+    age: int
+    hobbies: list[str]
+
+
+def get_full_name(student: StudentDict) -> str:
+    return f'{student.get("first_name")} {student.get("last_name")}'
+
+
+student1: StudentDict = {
+    "first_name": "John",
+    "last_name": "Doe",
+    "age": 18,
+    "hobbies": ["singing", "dancing"],
+}
+
+
+result  = get_full_name(student1)
+
+# print(result)
+
+
+
+# Adding type hints to tuples
+
+
+
+from typing import NamedTuple
+
+class StudentTuple(NamedTuple):
+    name: str
+    age: int
+
+john = StudentTuple("John Doe", 33)
+
+
+def student_info(student: StudentTuple) -> None:
+    name, age = student
+    print(f"Name: {name}\nAge: {age}")
+
+student_info(john)
+
+
+
+
+# Creating and using protocols
+
+# __init__ is a special Python method.
+
+# It is automatically called when you create an instance of the class.
+
+from typing import Protocol
+
+class HasBirthYear(Protocol):
+    def get_birthyear(self) -> int:
+        return self.birthyear
+
+class Person:
+    def __init__(self, name, birthyear):
+        self.name = name
+        self.birthyear = birthyear
+
+    def get_birthyear(self) -> int:
+        return self.birthyear
+
+def calc_age(current_year: int, data: HasBirthYear) -> int:
+    return current_year - data.get_birthyear()
+
+john = Person("john doe", 1996)
+print(calc_age(2021, john))
+
+
+# Annotating overloaded functions

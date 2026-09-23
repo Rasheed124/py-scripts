2869c27177328e2b6cb9190fee8c0e29bacd0ea7
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-09-23 18:22:39 +0100

    Reapply "added challenge file and completed level one"
    This reverts commit f36787c4d997f8d5fd7835b27279bd9d27d219ae.

---
+++ advanced/__init__.py
@@ -0,0 +1,9 @@
+# print(f'Invoking __init__.py for {__name__}')
+# A = ['quux', 'corge', 'grault']
+
+
+
+# second option
+
+# print(f'Invoking __init__.py for {__name__}')
+# import pkg.mod1, pkg.mod2
